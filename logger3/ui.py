41375f2a6659e32a744f0fa67045960e13c99f31
anna-marie-vos <amvos@outlook.com>
2017-06-28 18:07:35 +1200

    works

--- logger3/ui.py
+++ logger3/ui.py
@@ -8,20 +8,31 @@ class UI:
     def __init__(self, window):
         self.window = window
         self.timeit = False
-
+        self.interval = 5000
         self.window.wm_title("Timesheet Logger")
 
+        inputLabel = Label(window, text= "Set the time interval:")
+        inputLabel.grid(row = 1, column = 0 )
+
+        self.timeInput = IntVar()
+        timeEntry = Entry(window, textvariable = self.timeInput)
+        timeEntry.grid(row = 1, column = 1)
+
+        unitLabel = Label(window, text="seconds")
+        unitLabel.grid(row = 1, column = 2)
+
         startBtn = Button(window, text = "Start", width = 12, command=self.start)
-        startBtn.grid(row = 1, column = 0)
+        startBtn.grid(row = 2, column = 0)
 
         startBtn = Button(window, text = "Pause", width = 12, command=self.quitit)
-        startBtn.grid(row = 1, column = 1)
+        startBtn.grid(row = 2, column = 1)
 
         quitBtn = Button(window, text = "Quit", width = 12, command=self.window.destroy)
-        quitBtn.grid(row = 2, column = 0)
+        quitBtn.grid(row = 2, column = 2)
 
     def start(self):
         self.timeit=True
+        self.getInterval()
         self.timeLoop()
 
     def quitit(self):
@@ -31,7 +42,14 @@ class UI:
     def timeLoop(self):
         csv.checkIfCsvExist()
         if self.timeit:
-            self.window.after(5000, self.timeLoop)
+            self.window.after(self.interval, self.timeLoop)
+
+    def getInterval(self):
+        if self.timeInput.get() =="":
+            self.interval = 5000
+        else :
+            self.interval = self.timeInput.get()*1000
+            print(self.interval)
 
 
 root = Tk()
